e26a0166a383b6ccbd952995f40ced0cdc1347b0
Josephliao <gn02239440@gmail.com>
2017-10-25 22:52:37 +0800

    iframe

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 var request = require('request');
+const port = 80
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
     // app.use(bodyParser.urlencoded(
@@ -34,38 +36,47 @@ app.post('/test', (req, res, next) => {
 
 app.get('/confirm_order', (req, res, next) => {
     
-    var order_id = req.query.transactionId
+    var transactionId = req.query.transactionId
     
     confirmOrder(transactionId, function(err, result){
-    
-        res.json({
-            "result": result
-        })
+        console.log(result)
+        var transactionId = result.info.transactionId
+        var orderId = result.info.orderId
+        res.redirect(`/iframe_result.html?transactionId=${transactionId}&orderId=${orderId}`);
     
     })
 
 })
+
 
 app.post('/create_order', (req, res, next) => {
     
     var order_id = req.body.order_id
-
+    console.log(order_id)
     reserveOrder(order_id, function(err, result){
+        console.log("[1] reserveOrder")
+        if(err) {
+            console.log("[2] reserveOrder err")
+            console.log(err)
+            return res.json({
+                "paymentUrl": "Error"
+            })
+        }
 
+        console.log("[3] reserveOrder success")
+        console.log(result)
         res.json({
-            "paymentUrl": info.paymentUrl.web
+            "paymentUrl": result.info.paymentUrl.web
         })
 
     })
 })
 
-// heroku will set port via env PORT
-const port = process.env.PORT || 8080
-
-app.listen(process.env.PORT || 8080)
+app.listen(port)
 
 function reserveOrder(order_id, callback) {
     
+    console.log("[4] reserveOrder function")
     var options = {
         url: 'https://sandbox-api-pay.line.me/v2/payments/request',
         method: 'POST',
@@ -74,7 +85,7 @@ function reserveOrder(order_id, callback) {
             "productName" : "TapPay X LinePay",
             "amount" : 1,
             "currency" : "TWD",
-            "confirmUrl" : "https://yu-jack-slack-slack-bot.herokuapp.com/confirm_order",
+            "confirmUrl" : "https://linepay-test.tappaysdk.com/confirm_order",
             "orderId" : order_id
         },
         headers: { "content-type": "application/json", "X-LINE-ChannelId":"1540179966", "X-LINE-ChannelSecret":"bfd45f696e4421f39bb6d35f23475c3a" }
--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,18 @@
 $(function() {
     
+    var parser = new UAParser();
+    var device_os = parser.getResult().os.name;
+    console.log(device_os)
+    var linepayPaymentUrl = ""
     var $sendButton = $('#sendButton');
+    var order_id = makeid()
+    var url = window.location.href 
+    var path = window.location.pathname
+
+    document.getElementById("order_id").innerHTML = "Order ID : "+order_id
+
     $sendButton.click(function() {
         
-        var order_id = makeid()
         $.ajax({
             url: '/create_order',
             data: {"order_id": order_id},
@@ -11,8 +20,7 @@ $(function() {
             dataType: 'json',
 
             success: function(result) {
-                console.log(result.paymentUrl)
-                window.location(result.paymentUrl)
+                handleUrl(result.paymentUrl)
             },
 
             error: function(xhr, ajaxOptions, thrownError) {
@@ -23,14 +31,120 @@ $(function() {
 
     })
 
+
     function makeid() {
         var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var abcPossible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       
-        for (var i = 0; i < 10; i++)
-          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        for (var i = 0; i < 2; i++) {
+          text += abcPossible.charAt(Math.floor(Math.random() * abcPossible.length));
+        }
+
+        var numPossible = "0123456789"
+
+        for (var i = 0; i < 8; i++) {
+            text += numPossible.charAt(Math.floor(Math.random() * numPossible.length));
+        }
       
         return text;
     }
+
+
+    function handleUrl(paymentUrl){
+        if(device_os === "iOS" || device_os === "Android") {
+            redirect(paymentUrl)        
+        }else {
+            openIframe(paymentUrl)
+        }
+        
+    }
+        
+    function openIframe(paymentUrl){
+        linepayPaymentUrl = paymentUrl
+        document.body.style = ""
+        document.body.style = "background-color: rgba(0,0,0,0.8)"
+        var iframe = document.createElement('iframe')
+        iframe.id = "iframe_callback"
+        iframe.src = "https://linepay-test.tappaysdk.com/iframe_callback.html"
+        iframe.style = "position:relative; width:90%; height:700px; margin: -35px auto 0 auto; z-index: 1000;"
+        document.body.append(iframe)
+        window.addEventListener("message", function(event){
+            var data = JSON.parse(event.data)
+            if (data.event == "user_cancel") {
+                // Cancel
+                document.getElementById('iframe_callback').remove()
+                document.body.style = ""
+                alert("Cancel Payment!")
+
+            }else if(data.event == "payment_result") {
+                // Payment Result
+                document.getElementById('iframe_callback').remove()
+                document.body.style = ""
+        
+                // Redirect / Formsubmit To Finish Page.
+                window.location = "https://linepay-test.tappaysdk.com/result.html?transactionId="+data.transactionId+"&orderId="+data.orderId+""
+        
+            }else if(data.event == "get_payment_url") { 
+                document.getElementById("iframe_callback").contentWindow
+                .postMessage(JSON.stringify({
+                    event: "get_payment_url",
+                    paymentUrl: linepayPaymentUrl
+                }), "*")
+            }
+
+                   
+        }, false)
+                
+    }
+            
+    function redirect(paymentUrl){
+        
+        window.location=paymentUrl
+    
+    }
       
 })
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// outside website
+document.body.style = ""
+document.body.style = "background-color: rgba(0,0,0,0.8)"
+var iframe = document.createElement('iframe')
+iframe.id = "iframe-test"
+iframe.src = "https://tpdirect-demo.tappaysdk.com"
+iframe.style = "position:relative; width:70%; height:70%; margin: 200px auto 0 auto; z-index: 100; display: block;"
+document.body.prepend(iframe)
+window.addEventListener("message", function(event){
+    var data = JSON.parse(event.data)
+    if (data.event = "inside_done_then_post_to_outside") {
+        // do something
+        document.getElementById('iframe-test').remove()
+        document.body.style = ""
+        window.location = "https://www.tappaysdk.com/tch"
+    }
+}, false)
+// inside (iframe)
+function inside_done_then_post_to_outside() {
+    parent.postMessage(JSON.stringify({
+        event: 'inside_done_then_post_to_outside'
+    }), "*")
+}
+var button = document.createElement("button")
+button.addEventListener('click', inside_done_then_post_to_outside)
+button.textContent = "X"
+button.style = "float:right; color: black"
+document.body.prepend(button)
+
+*/
