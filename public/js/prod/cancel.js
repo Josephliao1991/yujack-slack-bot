f65a81af87ebbd55d0a8b86822b7782b36d9e84f
Josephliao <gn02239440@gmail.com>
2017-11-15 20:22:39 +0800

    no message

---
+++ public/js/prod/cancel.js
@@ -0,0 +1,13 @@
+$(function() {
+    
+    var $returnButton = $('#returnButton');
+    $returnButton.click(function() {
+        window.location = "https://linepay-test.tappaysdk.com/prod"
+    })
+
+      
+})
+
+
+
+
